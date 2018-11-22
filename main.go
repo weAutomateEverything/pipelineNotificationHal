f04f16d945146376c6eddcb50b82320687052349
Marc Arndt <marc@marcarndt.com>
2018-11-22 12:39:06 +0200

    removing statics

--- main.go
+++ main.go
@@ -1,25 +1,44 @@
 package main
 
 import (
-	"log"
-
+	"encoding/json"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"net/http"
+	"strings"
 )
 
 // Handler is executed by AWS Lambda in the main function. Once the request
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.CloudWatchEvent) (error) {
 
-	resp,err := request.Detail.MarshalJSON()
+	v := event{}
+	err := json.Unmarshal(request.Detail,&v)
 	if err != nil {
 		return err
 	}
-	log.Println(string(resp))
+
+	msg := fmt.Sprintf("Code Pipeline error detected:\n" +
+		"Pipeline: %v\n" +
+		"Stage: %v'n" +
+		"Action: %v",v.Pipeline,v.Stage,v.Action)
+
+	resp, err := http.Post("%v/api/alert/%v","application/text",strings.NewReader(msg))
+	if err != nil {
+		return err
+	}
+	resp.Body.Close()
 	return nil
 
 }
 
 func main() {
 	lambda.Start(Handler)
+}
+
+type event struct {
+	Pipeline string `json:"pipeline"`
+	Stage string `json:"stage"`
+	Action string `json:"action"`
 }
